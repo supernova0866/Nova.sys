ea028cc1c548aa89ca1f972c1e7836c9d53f3983
Nova <yash13579pro@gmail.com>
2026-06-12 00:49:03 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  OWNER_ID: '875703615099134013',
+  TOKEN: process.env.DISCORD_TOKEN,
+  CLIENT_ID: process.env.DISCORD_CLIENT_ID,
+  TURSO_URL: process.env.TURSO_URL,
+  TURSO_TOKEN: process.env.TURSO_TOKEN,
+  GENIUS_TOKEN: process.env.GENIUS_TOKEN,
+};
