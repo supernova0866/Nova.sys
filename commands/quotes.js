42bebbd5344bfcc9f6f4747f15301f728a1ec0a2
Nova <yash13579pro@gmail.com>
2026-06-14 03:26:29 +0530

    Create quotes.js

---
+++ commands/quotes.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { container, textDisplay, separator } = require('../utils/components');
+const { tieredPick } = require('../utils/tieredPick');
+const quotesData = require('../assets/quotes.json');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('quote')
+    .setDescription('Get a random quote')
+    .setIntegrationTypes([1])
+    .setContexts([0, 1, 2]),
+
+  async execute(interaction) {
+    const { entry, tier, category } = tieredPick(quotesData);
+
+    const components = [
+      textDisplay(`## Quote`),
+      separator(),
+      textDisplay(entry),
+    ];
+
+    if (tier === 'cat') {
+      components.push(separator());
+      components.push(textDisplay(`-# Source: The Cat.`));
+    } else if (tier === 'bot') {
+      components.push(separator());
+      components.push(textDisplay(`-# Source: Nova.sys Internal Archives.`));
+    }
+
+    await interaction.reply(container(components, interaction.user.id));
+  },
+};
